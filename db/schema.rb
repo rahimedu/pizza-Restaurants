77bb32160e4026eabbc982721ffc272301bc4402
rahimedu <edwin.duche82@gmail.com>
2022-10-06 23:59:45 -0500

    done

--- db/schema.rb
+++ db/schema.rb
@@ -8,6 +8,16 @@
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_10_01_194351) do
+  create_table "pizzas", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "restaurant_pizzas", force: :cascade do |t|
     t.integer "price"
     t.integer "pizza_id"
